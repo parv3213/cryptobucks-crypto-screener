7bed7de00e4b8a72e8f4b93383cf6ed7dd8a59ad
Parv3213 <parv3213@gmail.com>
2022-10-28 00:21:13 +0530

    Add empty Pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import './index.css'
-import App from './App'
+import Home from './Pages/Home'
+import Crypto from './Pages/Crypto'
+import Trending from './Pages/Trending'
+import Saved from './Pages/Saved'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    children: [
+      {
+        path: '/',
+        element: <Crypto />,
+      },
+      {
+        path: '/trending',
+        element: <Trending />,
+      },
+      {
+        path: '/saved',
+        element: <Saved />,
+      },
+    ],
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App />)
+root.render(<RouterProvider router={router} />)
