a6aa1f6f8fdb64ed98319ab64790855c5fbd3039
Parv3213 <parv3213@gmail.com>
2022-11-04 23:54:37 +0530

    Fix Logo

--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -6,7 +6,7 @@ const Logo = () => {
   return (
     <Link
       to="/"
-      className="absolute top-[1.5rem] left-[1.5rem] flex items-center text-sm text-cyan [text-decoration:none] sm:text-lg"
+      className="absolute top-[1.5rem] left-[1.5rem] flex items-center text-md text-cyan [text-decoration:none] sm:text-lg"
     >
       <img src={logoSvg} alt="CryptoBunks" />
       <span>CryptoBucks</span>
